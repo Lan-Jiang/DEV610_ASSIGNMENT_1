7c04f77dcdcba81f249e422199df26861a802d5b
LanJiang <30459778+Lan-Jiang@users.noreply.github.com>
2023-07-02 21:48:39 -0700

    Assignment 1 Chapter 1 initiate

---
+++ Chapter1/Exercise1_Stewie.java
@@ -0,0 +1,9 @@
+public class Exercise1_Stewie {
+    public static void main(String[] args) {
+        System.out.println("//////////////////////");
+        System.out.println("|| Victory is mine! ||");
+        System.out.print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+
+
+}
